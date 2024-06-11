369132158675c49e479d8cf70e8fee49132a4612
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-12 01:55:43 +0530

    set up

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Socials</h1>
-    </main>
+    <div>
+      <h1 className="head-text text-left">Home</h1>
+    </div>
   );
 }
---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware({
+   
+  publicRoutes: ["/","/api/webhook/clerk"],
+  ignoredRoutes: ["/api/webhook/clerk"],
+  });
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
