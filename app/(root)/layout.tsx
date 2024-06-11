b386dfc6da59ed0da9d2e740a089cf1ffab1b980
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-12 02:39:45 +0530

    setupsidebarLinks: sidebarLinks

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,6 +5,9 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 import "../globals.css";
+import Topbar from "@/components/shared/Topbar";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
