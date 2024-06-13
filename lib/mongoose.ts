08d67c90ac0b473b0ab9f1836e908a24835f3a6a
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-14 02:57:45 +0530

    backend setup

---
+++ lib/mongoose.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false; 
+
+export const connectToDB = async () => {
+ 
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
+
+  // If the connection is already established, return without creating a new connection.
+  if (isConnected) {
+    console.log("MongoDB connection already established");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
