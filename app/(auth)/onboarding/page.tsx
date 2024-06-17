d54c92a1c33ab65cbe68823cf1f87957ef5885ac
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-17 21:18:57 +0530

    error fix

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -3,12 +3,15 @@ import { redirect } from "next/navigation";
 
 
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null; 
 
-  const userInfo = {};
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
+
   
 
   const userData = {
