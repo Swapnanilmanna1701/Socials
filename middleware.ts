c81b2e26e9f5ad7c2bb7a4f8ba007bd92e837988
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-18 19:49:23 +0530

    issue fix

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware({
    
-  publicRoutes: ["/api/webhook/clerk"],
+  publicRoutes: ["/","/api/webhook/clerk"],
   ignoredRoutes: ["/api/webhook/clerk"],
   });
 
