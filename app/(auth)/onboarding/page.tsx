643e76407b66f6cd3e269ed645c2f58cc21aad5b
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-06-21 03:17:09 +0530

    issue fx

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -14,17 +14,17 @@ async function Page() {
   const userData = {
     id: user?.id,
     objectId: userInfo?._id,
-    username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
-    bio: userInfo?.bio || "",
-    image: userInfo?.image || user?.imageUrl,
+    username: userInfo ? userInfo?.username : user?.username,
+    name: userInfo ? userInfo?.name : user.firstName || "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user?.imageUrl,
   };
 
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text">Onboarding</h1>
       <p className="mt-3 text-base-regular text-light-2">
-        Complete your profile now, to use Threds.
+        Complete your profile now, to use Socials
       </p>
 
       <section className="mt-9 bg-dark-2 p-10">
